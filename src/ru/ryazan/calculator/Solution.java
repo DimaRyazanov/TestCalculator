b8c6ee6df2da58bc9c36d3163adfd5950b98687a
Ryazan <ryazangaming@gmail.com>
2019-05-19 19:22:46 +0500

    Диалог с пользователем

--- src/ru/ryazan/calculator/Solution.java
+++ src/ru/ryazan/calculator/Solution.java
@@ -1,7 +1,29 @@
 package ru.ryazan.calculator;
 
+import ru.ryazan.calculator.calculator.Calculator;
+import ru.ryazan.calculator.calculator.SimpleOperator;
+import ru.ryazan.calculator.exception.CalculateException;
+
 public class Solution {
     public static void main(String[] args) {
+        Calculator calculator = new SimpleOperator();
+
         ConsoleHelper.writeMessage("Простой калькулятор");
+
+        ConsoleHelper.writeMessage("Введите первое число:");
+        int first = ConsoleHelper.readInt();
+
+        ConsoleHelper.writeMessage("Введите второе число:");
+        int second = ConsoleHelper.readInt();
+
+        ConsoleHelper.writeMessage("Введите операцию (+, -, *, /");
+        try {
+            ConsoleHelper.writeMessage(String.valueOf(calculator.calculate(first, second, ConsoleHelper.readOperator())));
+        } catch (CalculateException e) {
+            ConsoleHelper.writeMessage("Ошибка расчета. Сорри бро!");
+        }
+
+        ConsoleHelper.writeMessage("Конец работы программы. Нажми Enter для выхода");
+        ConsoleHelper.read();
     }
 }
